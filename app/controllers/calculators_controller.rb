88df8a11b64d93ac02f23e0d242f92ba34a7d3ea
Shaher-11 <shahershamroukh@gmail.com>
2021-12-14 22:33:05 +0200

    Add claclute method

--- app/controllers/calculators_controller.rb
+++ app/controllers/calculators_controller.rb
@@ -1,6 +1,21 @@
-class CalculatorsController < ApplicationController
+# frozen_string_literal: true
 
+class CalculatorsController < ApplicationController
   def calculate
-  
+    a, b, operation = params.values
+    a = a.to_i
+    b = b.to_i
+    result = case operation
+             when 'SUM' then    a + b
+             when 'TIMES' then  a * b
+             when 'DEVIDE' then a / b
+             when 'SUB' then    a - b
+             else
+               "Error: Operation has an invalid value (#{operation})"
+             end
+
+    render json: {
+      "result": result
+    }
   end
 end
